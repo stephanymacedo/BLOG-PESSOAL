0a8616bb922fcf2aed7224402f4a71f7897f50dd
Stephanymacedo <stephanymaceo@outlook.pt>
2021-04-06 15:40:49 -0300

    security

---
+++ src/main/java/org/generation/StephanyBlog/Seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package org.generation.StephanyBlog.Seguranca;
+
+import java.util.Optional;
+
+
+
+import org.generation.StephanyBlog.Model.Usuario;
+import org.generation.StephanyBlog.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
+		Optional<Usuario>user=userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName+"not found."));
+	
+		return user.map(UserDetailsImpl:: new).get();
+	
+	}
+
+}
